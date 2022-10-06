c8b2fa52a17a98b73711294d36a99b8e6c360ab8
mnogcunha <112988834+mnogcunha@users.noreply.github.com>
2022-10-05 21:57:12 -0300

    Atualizando UsuarioController (Final)

--- ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
+++ ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
@@ -17,18 +17,14 @@ public class UsuarioController {
      * */
     @GetMapping("usuarios")
     public ArrayList<UsuarioModel> getAllUsuarios() {
-
 //        UsuarioModel u = new UsuarioModel();
 //        u.setId(1);
 //        u.setNome("Manoel");
 //        u.setEmail("manoel@gmail.com");
 //
 //        usuarios.add(u);
-
         return usuarios;
     }
-
-
     // o PostMapping permite associar o verbo POST com a rota /usuarios
     @PostMapping("usuarios")
     public UsuarioModel create(@RequestBody UsuarioModel usuario) {
@@ -50,6 +46,30 @@ public class UsuarioController {
                 return usuarios.get(i);
             }
         }
+        return null;
+    }
+    @DeleteMapping("usuarios/{id}")
+    public String delete(@PathVariable int id) {
+
+        // como achar o usuario a ser deletado?
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios.get(i).getId() == id) {
+                // achamos o usuario a ser deletado
+                usuarios.remove(i);
+                return "Usuario deletado com sucesso!";
+            }
+        }
+        return "Usuario não encontrado";
+    }
+    // busca por apenas um usuário (pelo id)
+    @GetMapping("usuarios/{id}")
+    public UsuarioModel getOne(@PathVariable int id){
+
+        for (int i = 0; i < usuarios.size(); i++){
+            if (usuarios.get(i).getId() == id){
+                return usuarios.get(i);
+            } // if
+        } // for
         return null;
     }
 }
