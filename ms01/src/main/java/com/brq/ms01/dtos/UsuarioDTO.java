d151fbb5753324e930aa5ece5199497ee0c777d8
mnogcunha <112988834+mnogcunha@users.noreply.github.com>
2022-10-24 21:56:44 -0300

    Atualizando ms01 em 24-10-2022

--- ms01/src/main/java/com/brq/ms01/dtos/UsuarioDTO.java
+++ ms01/src/main/java/com/brq/ms01/dtos/UsuarioDTO.java
@@ -4,10 +4,12 @@ import com.brq.ms01.models.UsuarioModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.modelmapper.ModelMapper;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
@@ -17,8 +19,11 @@ public class UsuarioDTO {
 
     @NotEmpty(message = "Campo não pode ser branco")
     @NotNull(message = "Preenchimento Obrigatório")
+    @Length(min=3, max=50, message = "O número de caracteres pode ser entre 3 e 50")
     private String nome;
     private String email;
+
+    @Pattern(regexp = "^\\([1-9]{2}\\) [9]{0,1}[6-9]{1}[0-9]{3}\\-[0-9]{4}$", message = "Telefone inválido")
     private String telefone;
 
     public UsuarioModel toModel(){
