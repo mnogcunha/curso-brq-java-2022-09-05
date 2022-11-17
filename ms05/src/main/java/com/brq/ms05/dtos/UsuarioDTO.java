0e46a35dadbddff3f4eb4ed3fcfbbcdefa82ae86
mnogcunha <112988834+mnogcunha@users.noreply.github.com>
2022-11-16 22:06:16 -0300

    Atualizando ms05 com dtos em 16-11-2022

--- ms05/src/main/java/com/brq/ms05/dtos/UsuarioDTO.java
+++ ms05/src/main/java/com/brq/ms05/dtos/UsuarioDTO.java
@@ -1,8 +1,13 @@
 package com.brq.ms05.dtos;
 
+import com.brq.ms05.models.UsuarioModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
@@ -11,6 +16,16 @@ public class UsuarioDTO {
 
     private String id;
 
+    @NotNull(message = "o campo nome não pode ser nulo")
+    @NotEmpty(message = "o campo nome não pode ser vazio")
     private String nome;
+
+    @NotNull(message = "o campo email não pode ser nulo")
+    @NotEmpty(message = "o campo email não pode ser vazio")
     private String email;
+
+    public UsuarioModel toModel(){
+        final var mapper = new ModelMapper();
+        return mapper.map(this, UsuarioModel.class);
+    }
 }
