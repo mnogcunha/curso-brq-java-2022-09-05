9747cdc44693667f448b9dfd2f8cd7736f07ee38
mnogcunha <112988834+mnogcunha@users.noreply.github.com>
2022-11-17 21:51:14 -0300

    Atualizando ms05 em 17-11-2022

--- ms05/src/main/java/com/brq/ms05/controllers/UsuarioController.java
+++ ms05/src/main/java/com/brq/ms05/controllers/UsuarioController.java
@@ -2,16 +2,17 @@ package com.brq.ms05.controllers;
 
 import com.brq.ms05.dtos.UsuarioDTO;
 import com.brq.ms05.mappers.UsuarioMapper;
-import com.brq.ms05.models.UsuarioModel;
 import com.brq.ms05.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "${endpointDoDanilo}")
 public class UsuarioController {
 
     @Autowired
@@ -20,8 +21,33 @@ public class UsuarioController {
     @Autowired
     private UsuarioMapper mapper;
 
-    @GetMapping(value = "usuarios")
+    @GetMapping
     public ResponseEntity<List<UsuarioDTO>> getAll(){
         return  ResponseEntity.ok().body( this.service.getAll() );
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> create(@Valid @RequestBody UsuarioDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body( service.create(dto.toModel())  );
+    }
+
+    @PatchMapping (value = "{id}")
+    public ResponseEntity<UsuarioDTO> update(
+            @PathVariable String id,
+            @Valid @RequestBody UsuarioDTO dto){
+        return ResponseEntity.ok().body(service.update(id, dto));
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable String id
+    ){
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<UsuarioDTO> getOne(@PathVariable String id){
+        return ResponseEntity.ok().body( service.getOne(id) );
     }
 }
