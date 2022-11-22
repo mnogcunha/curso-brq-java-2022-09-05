daa395c94ef6b019758095c2f18e05a631d3c487
mnogcunha <112988834+mnogcunha@users.noreply.github.com>
2022-11-22 20:51:58 -0300

    Atualizando ms05 pom.xml em 22-11-2022

--- ms05/src/main/java/com/brq/ms05/controllers/UsuarioController.java
+++ ms05/src/main/java/com/brq/ms05/controllers/UsuarioController.java
@@ -12,11 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-// Anotação do Swagger
 @ApiOperation(
         value = "API de Usuários"
 )
-
 @RestController
 @RequestMapping(value = "${endpointDoDanilo}")
 public class UsuarioController {
@@ -27,66 +25,44 @@ public class UsuarioController {
     @Autowired
     private UsuarioMapper mapper;
 
-    // Anotação do Swagger
     @ApiOperation(
             value = "retorna todos os usuários",
             notes = "retorna todos os usuários do Mongo"
     )
-
     @GetMapping
-    public ResponseEntity<List<UsuarioDTO>> getAll() {
-        return ResponseEntity.ok().body(this.service.getAll());
+    public ResponseEntity<List<UsuarioDTO>> getAll(){
+        return  ResponseEntity.ok().body( this.service.getAll() );
     }
 
     @PostMapping
-    public ResponseEntity<UsuarioDTO> create(@Valid @RequestBody UsuarioDTO dto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto.toModel()));
+    public ResponseEntity<UsuarioDTO> create(@Valid @RequestBody UsuarioDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body( service.create(dto.toModel())  );
     }
 
-    @PatchMapping(value = "{id}")
+    @PatchMapping (value = "{id}")
     public ResponseEntity<UsuarioDTO> update(
             @PathVariable String id,
-            @Valid @RequestBody UsuarioDTO dto) {
+            @Valid @RequestBody UsuarioDTO dto){
         return ResponseEntity.ok().body(service.update(id, dto));
     }
 
     @DeleteMapping(value = "{id}")
     public ResponseEntity<Void> delete(
             @PathVariable String id
-    ) {
+    ){
         service.delete(id);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping(value = "{id}")
-    public ResponseEntity<UsuarioDTO> getOne(@PathVariable String id) {
-        return ResponseEntity.ok().body(service.getOne(id));
+    public ResponseEntity<UsuarioDTO> getOne(@PathVariable String id){
+        return ResponseEntity.ok().body( service.getOne(id) );
     }
 
-    @GetMapping(value = "/nome/{nome}")
-    public ResponseEntity<List<UsuarioDTO>> findByNome(@PathVariable String nome) {
-
-        // TODO: Não esquecer do ResponseEntity
-        return ResponseEntity
-                .ok()
-                .body(service.findByNome(nome));
-    }
-    @GetMapping(value = "/find-by-nome/{nome}")
-    public ResponseEntity<List<UsuarioDTO>> findByNomeContains(@PathVariable String nome) {
-
-        // TODO: Não esquecer do ResponseEntity
-       return ResponseEntity
-               .ok()
-               .body(service.findByNomeContains(nome));
-    }
-
-    @GetMapping(value = "/find-by-nome/{nomeBusca}/by-email/{emailBusca}")
-    public ResponseEntity<List<UsuarioDTO>> findByNomeContainsAndEmailContains(@PathVariable String nomeBusca,
-                                                                               @PathVariable String emailBusca){
-        // TODO: Não esquecer do ResponseEntity
-        return ResponseEntity
-                .ok()
-                .body(service.findByNomeContainsAndEmailContains(nomeBusca, emailBusca));
+    @GetMapping(value = "find-by-nome/{nome}")
+    public ResponseEntity< List<UsuarioDTO> > findByNome(
+            @PathVariable String nome){
+        return ResponseEntity.ok().body( service.findByNome(nome) );
     }
 
     @GetMapping(value = "find-by-all-attrs/{input}")
