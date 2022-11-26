7ca259b50708f72bd7827cdd33570e74971e509a
mnogcunha <112988834+mnogcunha@users.noreply.github.com>
2022-11-25 22:12:36 -0300

    Criando ms06 em 25/11/2022

---
+++ ms06/src/main/java/com/brq/ms06/Ms06Application.java
@@ -0,0 +1,13 @@
+package com.brq.ms06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms06Application.class, args);
+	}
+
+}
