ae57e57e1d9468945d838d938d609faf8a81a123
mnogcunha <112988834+mnogcunha@users.noreply.github.com>
2022-12-12 21:50:51 -0300

    Atualizando ms06 testes unitários page em 12/12/2022

--- ms06/src/test/java/com/brq/ms06/services/UsuarioServiceTests.java
+++ ms06/src/test/java/com/brq/ms06/services/UsuarioServiceTests.java
@@ -8,10 +8,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -241,26 +248,150 @@ class UsuarioServiceTests {
         assertThat(response.get(0).getEmail()).isEqualTo(listModel.get(0).getEmail());
     }
 
-//    @Test
-//    void findByEmailTest(){
-//        String input = "busca";
-//        String id = "1";
-//        String nome = "nome";
-//        String email = "email";
-//        final var usuarioModel = createUsuarioModelMock(id, nome, email);
-//        final var listModel = Arrays.asList(usuarioModel);
-//
-//        // quando
-//        when(repository.findByEmail(String email)).thenReturn(listModel);
-//
-//        // então
-//        final var response = service.findByEmail(String email, int page, int limit, String orderBy, String direction););
-//
-//        // validar teste
-//        assertThat(response.get(0).getId()).isEqualTo(listModel.get(0).getId());
-//        assertThat(response.get(0).getNome()).isEqualTo(listModel.get(0).getNome());
-//        assertThat(response.get(0).getEmail()).isEqualTo(listModel.get(0).getEmail());
-//    }
+    @Test
+    void findByEmailTest() {
+        // Dado que
+
+        final var email = "email";
+        final var page = 0;
+        final var limit = 5;
+        final var direction = Sort.Direction.ASC;
+        final var orderBy = "id";
+        final var pageRequest =
+                PageRequest.of(page, limit, direction, orderBy );
+
+
+        final var pageUsuarioModel = getPageUsuarioModel();
+
+        // Quando
+
+        when(repository.findByEmail(email, pageRequest))
+                .thenReturn(pageUsuarioModel);
+
+        //então
+
+        final var response = service.findByEmail(email, page, limit, orderBy, "ASC");
+
+        // Verificar resultado
+
+        assertThat(response.getContent().size()).isEqualTo(1);
+        assertThat(response.getTotalElements()).isEqualTo(1L);
+
+    }
+
+    private Page<UsuarioModel> getPageUsuarioModel(){
+
+        Page<UsuarioModel> pageUsuarioModel = new Page<UsuarioModel>() {
+
+            UsuarioModel model = UsuarioModel
+                    .builder()
+                    .id("1")
+                    .nome("nome")
+                    .email("email")
+                    .build();
+
+            List<UsuarioModel> listEntity = Arrays.asList(model);
+
+            @Override
+            public Iterator<UsuarioModel> iterator() {
+
+
+                return (Iterator<UsuarioModel>) listEntity;
+            }
+
+            @Override
+            public Pageable previousPageable() {
+                // TODO Auto-generated method stub
+                return null;
+            }
+
+            @Override
+            public Pageable nextPageable() {
+                // TODO Auto-generated method stub
+                return null;
+            }
+
+            @Override
+            public boolean isLast() {
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+            @Override
+            public boolean isFirst() {
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+            @Override
+            public boolean hasNext() {
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+            @Override
+            public boolean hasContent() {
+                // TODO Auto-generated method stub
+                return true;
+            }
+
+            @Override
+            public Sort getSort() {
+                // TODO Auto-generated method stub
+                return null;
+            }
+
+            @Override
+            public int getSize() {
+
+                return listEntity.size();
+            }
+
+            @Override
+            public int getNumberOfElements() {
+
+                return listEntity.size();
+            }
+
+            @Override
+            public int getNumber() {
+
+                return 1;
+            }
+
+            @Override
+            public List<UsuarioModel> getContent() {
+
+                return listEntity;
+            }
+
+            @Override
+            public <U> Page<U> map(Function<? super UsuarioModel, ? extends U> converter) {
+                // TODO Auto-generated method stub
+                return null;
+            }
+
+            @Override
+            public int getTotalPages() {
+
+                return 1;
+            }
+
+            @Override
+            public long getTotalElements() {
+
+                return listEntity.size();
+            }
+        };
+
+        return pageUsuarioModel;
+    }
 
     private UsuarioModel createUsuarioModelMock(){
         UsuarioModel usuarioModel = new UsuarioModel();
