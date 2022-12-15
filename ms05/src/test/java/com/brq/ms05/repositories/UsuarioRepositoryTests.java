3f13f989efdb48be61f1a98aedb03dd254b53242
mnogcunha <112988834+mnogcunha@users.noreply.github.com>
2022-12-15 19:54:15 -0300

    Dockerfile no ms05 em 15/12/2022

--- ms05/src/test/java/com/brq/ms05/repositories/UsuarioRepositoryTests.java
+++ ms05/src/test/java/com/brq/ms05/repositories/UsuarioRepositoryTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
